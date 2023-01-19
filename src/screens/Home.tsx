416bbb27b8376392b8613d87198e65a62ff0665f
Felipe Alves <profelipealves@outlook.com>
2023-01-19 13:04:52 -0300

    add habitDays

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,24 @@
-import { View } from 'react-native';
+import { View, Text } from "react-native";
 
-export function Home(){
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+
+import { Header, DAY_SYZE } from "../components/Header";
+import { HabitDay } from "../components/HabitDay";
+
+export function Home() {
   return (
-    <View>
-      
+    <View className="flex-1 bg-background px-8 pt-16">
+      <Header />
+      <View className="flex-row mt-6 mb-2">
+        {weekDays.map((weekDay, i) => (
+          <Text 
+          className="text-zinc-400 text-xl font-bold text-center mx-1"
+          key={`${weekDay}-${i}`}
+          style={{ width: DAY_SYZE }}
+          >{weekDay}</Text>
+        ))}
+      </View>
+      <HabitDay />
     </View>
-  )
+  );
 }
