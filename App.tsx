d5822a8bb4e4e43f5cb507f9e8ce3523b0947368
Felipe Alves <profelipealves@outlook.com>
2023-01-19 11:26:44 -0300

    add Home

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { useFonts,
 Inter_400Regular,
 Inter_600SemiBold,
 Inter_700Bold,
 Inter_800ExtraBold } from '@expo-google-fonts/inter';
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,22 +22,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+    <>
+      <Home />
       <StatusBar barStyle='light-content' backgroundColor='transparent' translucent />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor:'#09090a',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontFamily: 'Inter_800ExtraBold',
-  }
-});
