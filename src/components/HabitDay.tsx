d9ec07453a0b245846c27fa502f004576c4486b9
Felipe Alves <profelipealves@outlook.com>
2023-01-21 19:37:52 -0300

    :bulb: update props

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -7,9 +7,13 @@ export const DAY_MARGIN_BETWEEN = 8;
 export const DAY_SIZE =
   (Dimensions.get("screen").width / WEEK_DAYS) - (SCREEN_HORIZONTAL_PADDING + 5);
 
-interface Props extends TouchableOpacityProps {};
+interface Props extends TouchableOpacityProps {
+  amountOfHabits: number;
+  amountCompleted: number;
+  date: Date;
+};
 
-export function HabitDay({ ...rest }: Props) {
+export function HabitDay({amountOfHabits = 0, amountCompleted = 0, date, ...rest }: Props) {
   return (
     <TouchableOpacity
       className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
