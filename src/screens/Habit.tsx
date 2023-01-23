18619922a1a64c4a19486d4c36bcfb44750427a4
Felipe Alves <profelipealves@outlook.com>
2023-01-23 17:10:36 -0300

    feat: add alert

--- src/screens/Habit.tsx
+++ src/screens/Habit.tsx
@@ -1,15 +1,30 @@
-import { View, ScrollView, Text } from "react-native";
+import { View, ScrollView, Text, Alert } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import { BackButton } from "../components/BackButton";
 import dayjs from "dayjs";
 import { ProgressBar } from "../components/ProgressBar";
 import { Checkbox } from "../components/Checkbox";
+import { useEffect, useState } from "react";
+import { api } from "../libs/axios";
+import { Loading } from "../components/Loading";
 
 interface Params {
   date: string;
 }
 
-export function Habit(){
+interface DayInfoProps {
+  completedHabits: string[];
+  possibleHabits: {
+    id: string;
+    title: string;
+  }[];
+}
+
+export function Habit() {
+  const [loading, setLoading] = useState(true);
+  const [dayInfo, setDayInfo] = useState<DayInfoProps | null>(null);
+  const [completedHabits, setCompletedHabits] = useState<string[]>([]);
+
   const route = useRoute();
   const { date } = route.params as Params;
 
@@ -17,30 +32,52 @@ export function Habit(){
   const dayOfWeek = parsedDate.format("dddd");
   const dayOfMonth = parsedDate.format("DD/MM");
 
+  async function fetchHabits() {
+    try {
+      setLoading(true);
+
+      const response = await api.get("/day", { params: { date } });
+      setDayInfo(response.data);
+      setCompletedHabits(response.data.completedHabits);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Não foi possível carregar os hábitos");
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchHabits();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
-    <View className='flex-1 bg-background px-8 pt-16'>
+    <View className="flex-1 bg-background px-8 pt-16">
       <ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 100 }}
       >
         <BackButton />
-        <Text className='mt-6 text-zinc-400 font-semibold text-base lowercase'>
+        <Text className="mt-6 text-zinc-400 font-semibold text-base lowercase">
           {dayOfWeek}
-          </Text>
-          <Text className='text-white font-extrabold text-3xl'>
-          {dayOfMonth}
-          </Text>
+        </Text>
+        <Text className="text-white font-extrabold text-3xl">{dayOfMonth}</Text>
 
-          <ProgressBar progress={40}/>
-          <Checkbox
-            title="Fazer exercícios"
-            checked={true}
-            />
+        <ProgressBar progress={40} />
 
-          <Checkbox
-            title="Fazer exercícios"
-            checked={false}
-            />
+        <View className="mt-6">
+          {dayInfo?.possibleHabits &&
+            dayInfo.possibleHabits.map((habit) => (
+              <Checkbox 
+              title={habit.title} 
+              key={habit.id}
+              checked={completedHabits.includes(habit.id)} />
+            ))}
+        </View>
       </ScrollView>
     </View>
   );
